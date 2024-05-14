dfb6bc29776b6616cbad29705c4af96c6801e959
Giles Atkinson <gatk555@gmail.com>
2024-05-14 19:02:38 +0100

    Add additional examples of Verilog co-simulation and share the Verilog source and large parts of the example circuits between Verilator and Icarus Verilog.  Verilog source file adc.v has improved style: all assignments in the always block are now non-blocking.

--- examples/xspice/verilator/adc.v
+++ examples/xspice/verilator/adc.v
@@ -1,15 +1,17 @@
 // Digital control for a successive approximation ADC with switched capacitors.
 
+`timescale 100ns/100ns
+
 module adc(Clk, Comp, Start, Sample, Done, Result);
    input wire                Clk, Comp, Start;
    output reg		     Sample, Done;
    output reg [Bits - 1 : 0] Result;
-
+   
    parameter		     Bits=6;
 
    reg [Bits - 1 : 0]	     SR;
    reg			     Running;
-
+   
    initial begin
       $display("ADC simulation starting");
       Done = 0;
@@ -22,14 +24,12 @@ module adc(Clk, Comp, Start, Sample, Done, Result);
       if (Running) begin
 	 if (Sample) begin
 	    Sample <= 0;
-	    SR[Bits - 1] = 1;
-	    Result[Bits - 1] = 1;
+	    SR[Bits - 1] <= 1;
+	    Result[Bits - 1] <= 1;
 	 end else if (SR != 0) begin
-	    if (Comp)
-	      Result &= ~SR;
-	    SR >>= 1;
-	    Result |= SR;
-	    if (SR == 0) begin
+	    Result <= (Comp ? (Result & ~SR) : Result) | (SR >> 1);
+	    SR <= SR >> 1;
+	    if (SR == 1) begin
 	       Running <= 0;
 	       Done <= 1;
 	    end
@@ -38,8 +38,8 @@ module adc(Clk, Comp, Start, Sample, Done, Result);
 	 Running <= 1;
 	 Sample <= 1;
 	 Done <= 0;
-	 SR = 0;
-	 Result = 0;
+	 SR <= 0;
+	 Result <= 0;
       end
    end
 endmodule
